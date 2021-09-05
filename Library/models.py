fd8a3feb72be7e35396e5f3f04b5e73b785ae6d2
EMSI-zero <emad.ferdowsi@outlook.com>
2021-09-05 05:26:17 -0400

    -Fix country bug
    Fix country field in author model

--- Library/models.py
+++ Library/models.py
@@ -9,14 +9,14 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     bio = models.TextField()
-    country = models.CharField
+    country = models.CharField(max_length=100)
     
     
     #to add later
         #awards  
         #languages 
 
-        
+
     def __str__(self) -> str:
         return self.name + ' ' + self.last_name
 
