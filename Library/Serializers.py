1d25f94abd0800cbf0b92a7a99ec61640cb08e61
EMSI-zero <emad.ferdowsi@outlook.com>
2021-09-05 05:58:42 -0400

    -Add Serializer class for data models
    Add Serializer class for Author, Book and Genre

--- Library/Serializers.py
+++ Library/Serializers.py
@@ -1,4 +1,23 @@
+from django.db import models
 from rest_framwork import serializers
 from .models import Author, Genre , Book
 
 
+#serializer class for Author data model        
+class AuthorSerializer(serializers.ModalSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+#serializer class for Genre data model        
+class GenreSerializer(serializers.ModalSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+        
+#serializer class for Book data model        
+class BookSerializer(serializers.ModalSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        
