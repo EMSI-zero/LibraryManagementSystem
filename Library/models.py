bae783bfc127b74c528bb2468dfe6424c4ccf497
EMSI-zero <emad.ferdowsi@outlook.com>
2021-09-05 03:14:54 -0400

    -Add author model
    add author model representing author and holding author data

--- Library/models.py
+++ Library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+# Model Representing Author
+class Author:
+    
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    country = models.CharField
+    
+    
+    #to add later
+    #awards  
+    #languages 
