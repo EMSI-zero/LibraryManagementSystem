e97e43ec4e116d3c2f307a1f906afac5c08315f2
EMSI-zero <emad.ferdowsi@outlook.com>
2021-09-05 06:09:32 -0400

    -Add book-list url and view
    add book list url to urlpatterns and book view responding serializer

--- Library/urls.py
+++ Library/urls.py
@@ -6,5 +6,6 @@ from . import views #import views file to use views in url patterns
 
 urlpatterns = [
     path('api/' , views.apiOverview), #domain/api
-    path('author-list' , views.authorList , name="author-list")
+    path('author-list' , views.authorList , name="author-list"),
+    path('book-list' , views.bookList , name="book-list")
 ]
--- Library/views.py
+++ Library/views.py
@@ -1,4 +1,4 @@
-from Library.Serializers import AuthorSerializer
+from Library.Serializers import AuthorSerializer, BookSerializer
 from django.shortcuts import render
 
 
@@ -14,10 +14,24 @@ def apiOverview(request):
     return Response("Hello!")
 
 
+#Author Views
+
+#author list view. lists the author and responses using serializer.
 @api_view(['GET'])
 def authorList(request):
     authors = Author.objects.all()
     serializer = AuthorSerializer(authors , many=True)
     return Response(serializer.data)
+
+
+#Book Views
+
+#Book list view. lists the Book and responses using serializer.
+@api_view(['GET'])
+def bookList(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books , many=True)
+    return Response(serializer.data)
+
 
 
